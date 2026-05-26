06ecfebb5ed7c7a4ed51c227fd650897263f546e
CristianGodoy81 <cristiangodoy81@gmail.com>
2026-05-26 17:33:32 -0300

    variables de entorno

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // Utilizamos la variable de entorno de Vite, o la de localhost como respaldo
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
 });
 
 // interceptor token
